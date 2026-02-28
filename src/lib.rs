951e33dbde172495bf9dc177bc5f09c796e30a42
Jan Danielsson <jan@void>
2026-02-28 21:24:06 +0100

    Documentation infrastructure updates; add support for feature labels on docs.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
 mod eventmsgs;
 
 #[cfg(feature = "filtering")]
+#[cfg_attr(docsrs, doc(cfg(feature = "filtering")))]
 pub mod filtering;
 
 use std::convert::TryInto;
